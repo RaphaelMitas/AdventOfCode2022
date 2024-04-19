fa0173f11d8dd27b849a6d4a1017b32e8c4ab28a
Raphaël Mitas <raphael.mitas@web.de>
2024-04-19 22:58:34 +0200

    advent 4 part 2

--- 4-camp-cleanup/src/index.ts
+++ 4-camp-cleanup/src/index.ts
@@ -7,15 +7,21 @@ interface Range {
 }
 
 function parseRange(rangeStr: string): Range {
-  const [start, end] = rangeStr.split("-").map(num => Number(num));
+  const [start, end] = rangeStr.split("-").map((num) => Number(num));
   return { start, end };
 }
 
+//PART 1
 function isContained(a: Range, b: Range): boolean {
   return (
     (a.start <= b.start && a.end >= b.end) ||
     (b.start <= a.start && b.end >= a.end)
   );
+}
+
+//PART 2
+function overlaps(a: Range, b: Range): boolean {
+  return a.start <= b.end && a.end >= b.start;
 }
 
 function processAssignments(filePath: string): void {
@@ -25,17 +31,24 @@ function processAssignments(filePath: string): void {
   });
 
   let count = 0;
+  let countOverlaps = 0;
 
   reader.on("line", (line: string) => {
     const ranges = line.split(",").map((range) => parseRange(range));
     if (isContained(ranges[0], ranges[1])) {
       count++;
     }
+    if (overlaps(ranges[0], ranges[1])) {
+      countOverlaps++;
+    }
   });
 
   reader.on("close", () => {
     console.log(
       `Number of assignment pairs where one range fully contains the other: ${count}`
+    );
+    console.log(
+      `Number of assignment pairs where the ranges overlap: ${countOverlaps}`
     );
   });
 }
