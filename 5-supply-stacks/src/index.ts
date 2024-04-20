87f8971d2ba54acf488e08b1bd690aaa158e9a47
Raphaël Mitas <raphael.mitas@web.de>
2024-04-20 14:18:53 +0200

    advent 5 part 2

--- 5-supply-stacks/src/index.ts
+++ 5-supply-stacks/src/index.ts
@@ -30,7 +30,8 @@ function parseInput(filePath: string): Promise<[string[][], Move[]]> {
         parsingStacks = false;
       }
 
-      if (parsingStacks) { //parse stacks
+      if (parsingStacks) {
+        //parse stacks
         for (let i = 0; i < line.length; i += 4) {
           let segment = line.slice(i, i + 3).trim();
           const stackIndex = i / 4;
@@ -42,7 +43,8 @@ function parseInput(filePath: string): Promise<[string[][], Move[]]> {
             stacks[stackIndex].push(segment);
           }
         }
-      } else { //parse moves
+      } else {
+        //parse moves
         const match = line.match(/move (\d+) from (\d+) to (\d+)/);
         if (match) {
           moves.push({
@@ -67,6 +69,7 @@ function parseInput(filePath: string): Promise<[string[][], Move[]]> {
 }
 
 /**
+ * PART 1
  * Simulate crates movement
  * @param stacks stacks of crates
  * @param moves moves to simulate
@@ -92,7 +95,35 @@ async function simulateCratesMovement(
   return stacks;
 }
 
+/**
+ * PART 2
+ * Simulate crates movement
+ * @param stacks stacks of crates
+ * @param moves moves to simulate
+ * @returns final order of crates as 2D array
+ * @async
+ */
+async function simulateCratesMovement2(
+  stacks: string[][],
+  moves: Move[]
+): Promise<string[][]> {
+  for (const move of moves) {
+    const crates = stacks[move.fromStack - 1].splice(
+      stacks[move.fromStack - 1].length - move.quantity
+    );
+    stacks[move.toStack - 1].push(...crates);
+  }
+
+  return stacks;
+}
+
+//Log PART 1
 parseInput("./src/input.txt")
   .then(([stacks, moves]) => simulateCratesMovement(stacks, moves))
   .then((stacks) => stacks.reduce((acc, stack) => `${acc}${stack.pop()}`, ""))
-  .then(console.log);
+  .then((output) => console.log(`PART 1: ${output}`));
+
+parseInput("./src/input.txt")
+  .then(([stacks, moves]) => simulateCratesMovement2(stacks, moves))
+  .then((stacks) => stacks.reduce((acc, stack) => `${acc}${stack.pop()}`, ""))
+  .then((output) => console.log(`PART 2: ${output}`));
